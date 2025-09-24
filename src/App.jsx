4de762a347660e403ba885d5bd2fe3b282b2aee1
Purna <purnapsch@gmail.com>
2025-09-24 10:27:06 +0530

    code update-06

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DashBoard from "./pages/DashBoard";
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <DashBoard /> // Main dashboard component
+      <DashBoard />
     </div>
   );
 }
